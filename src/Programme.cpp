31762001fa755fc0b5eb40b7a09d58f0856481db
ChristopherAparicio <christopher.aparicio@free.fr>
2017-03-27 08:27:46 +0200

    Ajout de .cpp et .h de la classe Programme

---
+++ src/Programme.cpp
@@ -0,0 +1,39 @@
+/*************************************************************************
+PLD COMP
+Programme.cpp  -  Description
+-------------------
+début                : 20/03/2017
+copyright            : (C)2017 par H4114
+*************************************************************************/
+
+//---------- Réalisation de la classe Programme (fichier Programme.cpp) --
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "Programme.h"
+
+//---------------------------------------------------- Variables de classe
+
+//----------------------------------------------------------- Types privés
+
+
+//----------------------------------------------------------------- PUBLIC
+//-------------------------------------------------------- Fonctions amies
+
+//----------------------------------------------------- Méthodes publiques
+
+//----- Constructeur
+Programme::Programme()
+{}// Bloc vide
+//----- Fin constructeur
+
+//----- Destructeur
+Programme::~Programme()
+{}// Bloc vide
+//----- Fin destructeur
