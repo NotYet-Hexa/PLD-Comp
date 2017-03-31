1326506bab3ec3b0d03e677be886c8a5e3859a4e
SchroterQuentin <quentin.schroter@numericable.fr>
2017-03-31 10:56:22 +0200

    add travis test result

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 nbArgs=`wc -w <<< "${*:2}"`
+message=""
+fail=0
 
 if [ $# -eq 0 ]
 then
@@ -41,11 +43,42 @@ run_test() {
     if [ "$diff" == "" ]
     then
         echo -e "→ `basename $1 | sed -n "s/^\(.*\).test$/\1/p"` \xE2\x9C\x93"
+        message+="\n → `basename $1 | sed -n "s/^\(.*\).test$/\1/p"` ok"
     else
         echo "$1 fail"
         echo "$diff"
-        exit 1
+
+        message+="\n$1 fail"
+        message+="\n$diff"
+        fail=1
     fi
+}
+
+format_message() {
+    color="#36a64f"
+    title="Tests Successful"
+    titlelink="https://travis-ci.org/NotYet-Hexa/PLD-Comp"
+    text=`echo "$message" | sed 's/\\\x/%/g'`
+    # text=`echo "$message" | sed 's%\\\x%\\\\\\\x%g'`
+    if [ $fail -eq 1 ]
+    then
+        color="#ff0000"
+        title="Tests Fail"
+    fi
+
+    json='{'
+    json+='"attachments": ['
+    json+='    {'
+    json+='        "color": "'$color'",'
+    json+='        "author_name": "TestRunner",'
+    json+='        "title": "'$title'",'
+    json+='        "title_link": "'$titlelink'",'
+    json+='        "text": "'$text'"'
+    json+='    }'
+    json+=']'
+    json+='}'
+
+    echo "$json"
 }
 
 if [ $nbArgs -gt 0 ]
@@ -78,6 +111,7 @@ then
             exit 1
         fi
     done
+    format_message > slack_result.json
 else
     tests=`find tests -type f -name '*.test'`
     nbtest=`wc -w <<< "$tests"`
@@ -95,4 +129,5 @@ else
             echo -e "$test \t was ignored → $resultname \t missing"
         fi
     done
+    format_message > slack_result.json
 fi
