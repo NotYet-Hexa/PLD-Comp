c55eba5497789003bafe9afa93ad802502447c13
SchroterQuentin <quentin.schroter@numericable.fr>
2017-04-03 18:14:18 +0200

    update notification slack

---
+++ slack.sh
@@ -0,0 +1,25 @@
+
+send_message(){
+    curl -X POST -H "Content-type: application/json" --data @slack_result.json  https://hooks.slack.com/services/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
+}
+
+
+if [ -f slack_result.json ]
+then
+    send_message
+else
+    color="#ff0000"
+    commit_info=`git log -1 --pretty=format:"*%cn* : _%s_ <https://github.com/NotYet-Hexa/PLD-Comp/commit/%h|#%h>" 2>/dev/null`
+    title="Build Failed !"
+    text=`make | tail`
+    json='{'
+    json+='"text": "'$commit_info'",'
+    json+='"attachments": ['
+    json+='    {'
+    json+='        "color": "'$color'",'
+    json+='        "title": "'$title'",'
+    json+='        "text": "'$text'"'
+    json+='    }'
+    json+=']'
+    json+='}'
+fi
