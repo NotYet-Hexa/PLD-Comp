0c457c3064ec199e5037ceceefc2bfa692ddf935
SchroterQuentin <quentin.schroter@numericable.fr>
2017-04-03 20:15:49 +0200

    display error in slack from make if build failed

--- slack.sh
+++ slack.sh
@@ -4,21 +4,37 @@ send_message(){
 }
 
 
-if [ -f slack_result.json ]
+if [ -e slack_result.json ]
 then
     send_message
 else
     color="#ff0000"
     commit_info=`git log -1 --pretty=format:"*%cn* : _%s_ <https://github.com/NotYet-Hexa/PLD-Comp/commit/%h|#%h>" 2>/dev/null`
     title="Build Failed !"
-    text=`make 2>&1 | tail`
+    text_tmp=`make 2>&1 | tail | sed -e 's%"%\x27%g' -e 's%\^%%g' -e 's/^ *//g' | tr -d '\r'`
+    text=""
+
+    # make 2>&1 | tail | od -c
+
+    while read line
+    do
+        if [[ ! -z "$line" || "$line" = " "* ]]
+        then
+            text+="$line""\\n"
+        fi
+    done <<< "$text_tmp"
+
+    # echo -ne "$text"
+
+    # echo "$text" | od -c
+
     json='{'
-    json+='"text": "'$commit_info'",'
+    json+='"text": "'"$commit_info"'",'
     json+='"attachments": ['
     json+='    {'
-    json+='        "color": "'$color'",'
-    json+='        "title": "'$title'",'
-    json+='        "text": "'$text'"'
+    json+='        "color": "'"$color"'",'
+    json+='        "title": "'"$title"'",'
+    json+='        "text": "'"$text"'"'
     json+='    }'
     json+=']'
     json+='}'
